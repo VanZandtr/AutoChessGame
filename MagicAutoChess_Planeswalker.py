36693c09a49ccf00853e26fffb30d4ed6dfdf56e
AD\rlv220 <rlv220@lehigh.edu>
2019-12-16 11:41:13 -0500

    intial code, weird mousedown/onclick behavior

---
+++ MagicAutoChess_Planeswalker.py
@@ -0,0 +1,4 @@
+class Planeswalker:
+    def __init__(self):
+        #name, health, uptick, downtick, ultimate
+        self.planeswalkers = [['Chandra', '20', 2, -1, -7], ['Jace', '20', 2, 0, -8]]
