0a5b1671ceadcdd35d17e565d0cc36698b4fa867
AD\rlv220 <rlv220@lehigh.edu>
2019-12-17 11:36:24 -0500

    added background, buyphase, loyalty increase button, button functionalities, card objects, card addition and removal

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 let pws = [];
+let cards = [];
+
 let jace = {
  name: 'Jace',
- health: '20',
- loyalty: '2',
- up: '2',
- down: '0',
- ult: '8',
+ health: 20,
+ loyalty: 2,
+ up: -2,
+ down: 0,
+ ult: -8,
 };
 pws.push(jace);
 
 let chandra = {
  name: 'Chandra',
- health: '20',
- loyalty: '4',
- up: '1',
- down: '-2',
- ult: '7',
+ health: 20,
+ loyalty: 2,
+ up: -1,
+ down: -2,
+ ult: -7,
 };
 pws.push(chandra)
 
+let snapcaster = {
+    name: "Snapcaster Mage",
+    attack: 1,
+    health: 2,
+    ability: "flash",
+};
+cards.push(snapcaster);
+let buy = false;
+let bought = 0;
+
+
+
 function start() {
+
  //clear screen
  document.body.innerHTML = '';
+ document.body.style.background = "url('background.png')";
 
  //get a random planeswalker from list
  ran_num = Math.floor(Math.random()*pws.length)
@@ -138,6 +154,10 @@ function chosen_planeswalker(planeswalker){
  newdiv1.className = "Face";
  var textnode = document.createTextNode(planeswalker.name);
  newdiv1.appendChild(textnode);
+ newdiv1.ondragover = function(event){
+     event.preventDefault();
+     buy = true;
+ }
  
  var health_button = document.createElement("BUTTON");
  health_button.classList.add("HB");
@@ -150,15 +170,23 @@ function chosen_planeswalker(planeswalker){
  var up_button = document.createElement("BUTTON");
  up_button.classList.add("UB");
  up_button.innerHTML = planeswalker.up;
+ up_button.onclick = function() {
+    up_button_onclick();
+};
  
  var down_button = document.createElement("BUTTON");
  down_button.classList.add("DB");
  down_button.innerHTML = planeswalker.down;
+ down_button.onclick = function() {
+    down_button_onclick();
+};
  
  var ult_button = document.createElement("BUTTON");
  ult_button.classList.add("ULTB");
  ult_button.innerHTML = planeswalker.ult;
-
+ ult_button.onclick = function() {
+    ult_button_onclick();
+};
 
  document.body.appendChild(newdiv1);
  document.body.appendChild(health_button);
@@ -167,6 +195,85 @@ function chosen_planeswalker(planeswalker){
  document.body.appendChild(down_button);
  document.body.appendChild(ult_button);
  
-
+ buyphase(planeswalker);
+ console.log(planeswalker.loyalty);
 
 }
+
+function up_button_onclick(){
+ console.log("up clicked");
+}
+
+function down_button_onclick(){
+console.log("down clicked");
+}
+
+function ult_button_onclick(){
+console.log("ult clicked");
+}
+
+function buyphase(planeswalker){
+ 
+ for(i = 0; i <=7; i++){
+    var ran_num = Math.floor(Math.random()*cards.length);
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let id = i;
+    
+    var get_card = cards[ran_num];
+    
+    card.innerHTML = get_card.name;
+    if(i == 0){
+        card.style.marginLeft = "100px";
+    }
+    card.setAttribute('draggable', true);
+    card.ondragstart = function(){
+        card_onclick();
+    }
+    card.ondragend = function(){
+        card_upclick(card, get_card);
+    }
+    document.body.appendChild(card);
+    childindex = (Array.prototype.indexOf.call(card.parentNode.children, card));
+    document.body.childNodes[childindex].innerHTML += childindex;
+    
+
+    
+ }
+
+ var add_loyalty = document.createElement("BUTTON");
+ add_loyalty.classList.add("ADDL");
+ add_loyalty.innerHTML = "LOYALTY BUTTON";
+ add_loyalty.onclick = function(planeswalker) {
+  add_loyalty_onclick();
+ };
+ document.body.appendChild(add_loyalty);
+}
+
+function add_loyalty(planeswalker){
+ newLoyalty = planeswalker.loyalty + 1;
+ planeswalker.loyalty = newLoyalty;
+}
+
+function card_onclick(){
+ console.log("drag start");
+}
+
+function card_upclick(child, card){
+ console.log("drag end");
+ console.log(document.body.childNodes);
+ console.log("num bought" + bought);
+ if(buy == true){
+     let index = Array.prototype.indexOf.call(document.body.children, child);
+     console.log(index);
+    document.body.removeChild(document.body.childNodes[index]);
+    bought = 0;
+    childindex -=1;
+ }
+
+
+ buy = false;
+}
+
+//MAKE SURE TO SET BOUGHT TO 0!!!!!!
